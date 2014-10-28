6732d196484314600d45f3ecc070c8afb96962ff
JEBurgman <jan-eric.burgman.win14@utb.hermods.se>
2014-10-28 23:24:10 +0100

    Add ChessBoard class

---
+++ ChessGameConsoleApplication/ChessBoard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameConsoleApplication
+{
+    /// <summary>
+    /// This class contains all the UI related attributes and operations
+    /// </summary>
+    class ChessBoard
+    {
+        // Fields
+
+        // Properties
+
+        // Constuctors
+        public ChessBoard() { }
+
+        // Methods
+
+        /// <summary>
+        /// Start the gameloop and initialize the chessboard
+        /// </summary>
+        internal void Initialize()
+        {
+            // Call the Print method
+            Print();
+        }
+
+        void Print()
+        {
+            Console.WriteLine(this.GetType().Namespace);
+        }
+    }
+}
--- ChessGameConsoleApplication/Program.cs
+++ ChessGameConsoleApplication/Program.cs
@@ -8,8 +8,17 @@ namespace ChessGameConsoleApplication
 {
     class Program
     {
+        /// <summary>
+        /// The application entry point
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            // Make the ChessBoard
+            var chessBoard = new ChessBoard();
+
+            // Initialize the ChessBoard
+            chessBoard.Initialize();
         }
     }
 }
