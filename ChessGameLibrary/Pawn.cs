77388e706ce7195d31f0fef42a114e38cb58786d
GustavGenell <genell@live.se>
2014-10-30 15:51:40 +0100

    Added some class files

---
+++ ChessGameLibrary/Pawn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameLibrary
+{
+    class Pawn : ChessPiece
+    {
+        private bool startPosition { get; set; }
+
+        //return?
+        public void ValidMove(Position pos)
+        {
+            //Current Position
+            int y = pos.PositionY;
+            int x = pos.PositionX;
+
+
+            //..
+        }
+    }
+}
---
+++ ChessGameLibrary/Position.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameLibrary
+{
+    class Position : ChessPiece
+    {
+        public int PositionY { get; set; }
+        public int PositionX { get; set; }
+    }
+}
