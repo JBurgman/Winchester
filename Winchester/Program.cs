0a67637ae76b5ccb61634ff84975f5e3943a50d0
Linus <linusekdahl@gmail.com>
2014-10-30 15:00:13 +0100

    Added the complete main program code from my own personal chessgame.

--- Winchester/Program.cs
+++ Winchester/Program.cs
@@ -6,12 +6,25 @@ using System.Threading.Tasks;
 
 namespace Winchester
 {
-    class Program
+    class MainGame
     {
         static void Main(string[] args)
         {
-            //Test ----
-            //Emmys ändring
+            Random xRandom = new Random();
+            ChessBoard c = new ChessBoard();
+            c.DisplayBoard();
+            Console.ReadKey();
+
+            while (true)
+            {
+                while (!c.MovePiece(xRandom.Next(0, 8), xRandom.Next(0, 8), xRandom.Next(0, 8), xRandom.Next(0, 8)))
+                {
+
+                }
+                c.DisplayBoard();
+                Console.WriteLine("Pieces: " + c.CountPieces());
+                Console.ReadKey();
+            }
         }
     }
 }
