5dcc1bcbbcd97263c65a830a1dc394755ce8675f
Emmy Andersson <emmy.andersson.win14@utb.hermods.se>
2014-10-31 10:41:58 +0100

    ChessGame Logger

---
+++ ChessGameConsoleApplication/ChessGame.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameConsoleApplication
+{
+    class ChessGame
+    {
+
+        public int[] Players { get; set; }
+        private int turn;
+
+
+
+        public void StartGame()
+        {
+            //Logik för att starta spelet.
+        }
+
+        public int CountTurns()
+        {
+            // Logik för turns
+            return 0;
+        }
+
+        public void InitiateChessPieceList()
+        {
+            // Logik för att intitiera chesspiece listan, från klassen Pieces. 
+        }
+    }
+}
---
+++ ChessGameConsoleApplication/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameConsoleApplication
+{
+    class Logger
+    {
+        List<string> LogList = new List<string>();
+
+        public void Log()
+        {
+            // Logik för att logga information.
+        }
+    }
+}
