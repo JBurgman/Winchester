5aa872aff360b778bbceeb2a29adb7e3a7f3eac6
JEBurgman <jan-eric.burgman.win14@utb.hermods.se>
2014-11-03 13:54:53 +0100

    Uppdate Draw method

--- ChessGameConsoleApplication/Tiles.cs
+++ ChessGameConsoleApplication/Tiles.cs
@@ -22,16 +22,27 @@ namespace ChessGameConsoleApplication
         public override void Draw()
         {
 
-            for (int x = 0; x < Width; x++)
+            for (int i = 0; i < this.Width; i++)
             {
-                for (int y = 0; y < Height; y++)
+
+                for (int j = 0; j < this.Height; j++)
                 {
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    Console.SetCursorPosition(StartPosition.X + x, StartPosition.Y + y);
-                    Console.WriteLine("#");
+                    if (((i % 2 == 0) && (j % 2 == 0)) || ((i % 2 == 1) && (j % 2 == 1))) // Om bägge koordinaterna är udda eller bägge koordinaterna är jämna
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    }
+
+
+                    Console.SetCursorPosition(StartPosition.X + i, StartPosition.Y + j);
+
+
+                    Console.Write("█");
                 }
-             
-                
+                Console.WriteLine();
             }
         }
     }
