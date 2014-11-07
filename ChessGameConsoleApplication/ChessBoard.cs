b7b5f5b5e40d5078ebaeca58cebfa790c9f6e79c
Emmy Andersson <emmy.andersson.win14@utb.hermods.se>
2014-11-07 14:18:15 +0100

    All pieces on board. Unmoveable

--- ChessGameConsoleApplication/ChessBoard.cs
+++ ChessGameConsoleApplication/ChessBoard.cs
@@ -55,14 +55,51 @@ namespace ChessGameConsoleApplication
                 //Trying to see how it will look like
                 DrawChessPiece(new PieceSymbol(position, ConsoleColor.White, "P"));  // This pawn is the only one who can move, right now. 
 
-                DrawChessPiece(new PieceSymbol(new Position(1,1), ConsoleColor.White, "P"));  // These Pawns can not move.  
-                DrawChessPiece(new PieceSymbol(new Position(2,1), ConsoleColor.White, "P")); //
-                DrawChessPiece(new PieceSymbol(new Position(3,1), ConsoleColor.White, "P")) // 
 
-                DrawChessPiece(new PieceSymbol(new Position(4,3), ConsoleColor.White, "R")); //These three can not move. Only testing to see colours and letters.
-                DrawChessPiece(new PieceSymbol(new Position(2,4), ConsoleColor.Red, "K"));  // 
-                DrawChessPiece(new PieceSymbol(new Position(4, 7),ConsoleColor.Red,"Q"));  //
+                // White side, unmoveable pieces.
+
+                DrawChessPiece(new PieceSymbol(new Position(1,1), ConsoleColor.White, "P"));
+                DrawChessPiece(new PieceSymbol(new Position(2,1), ConsoleColor.White, "P")); 
+                DrawChessPiece(new PieceSymbol(new Position(3,1), ConsoleColor.White, "P"));
+                DrawChessPiece(new PieceSymbol(new Position(4,1), ConsoleColor.White, "P"));
+                DrawChessPiece(new PieceSymbol(new Position(5,1), ConsoleColor.White, "P"));
+                DrawChessPiece(new PieceSymbol(new Position(6,1), ConsoleColor.White, "P"));
+                DrawChessPiece(new PieceSymbol(new Position(7,1), ConsoleColor.White, "P"));
+
+                DrawChessPiece(new PieceSymbol(new Position(7, 0), ConsoleColor.White, "R"));
+                DrawChessPiece(new PieceSymbol(new Position(6, 0), ConsoleColor.White, "H"));
+                DrawChessPiece(new PieceSymbol(new Position(5, 0), ConsoleColor.White, "B"));
+                DrawChessPiece(new PieceSymbol(new Position(4, 0), ConsoleColor.White, "K"));
+                DrawChessPiece(new PieceSymbol(new Position(3, 0), ConsoleColor.White, "Q"));
+                DrawChessPiece(new PieceSymbol(new Position(2, 0), ConsoleColor.White, "B"));
+                DrawChessPiece(new PieceSymbol(new Position(1, 0), ConsoleColor.White, "H"));
+                DrawChessPiece(new PieceSymbol(new Position(0, 0), ConsoleColor.White, "R"));
+
+
+
+                //RED (black) Side. Unmoveable pieces.. 
+
+
+                DrawChessPiece(new PieceSymbol(new Position(0, 6), ConsoleColor.Red, "P"));
+                DrawChessPiece(new PieceSymbol(new Position(1, 6), ConsoleColor.Red, "P")); 
+                DrawChessPiece(new PieceSymbol(new Position(2, 6), ConsoleColor.Red, "P"));
+                DrawChessPiece(new PieceSymbol(new Position(3, 6), ConsoleColor.Red, "P"));
+                DrawChessPiece(new PieceSymbol(new Position(4, 6), ConsoleColor.Red, "P"));
+                DrawChessPiece(new PieceSymbol(new Position(5, 6), ConsoleColor.Red, "P"));
+                DrawChessPiece(new PieceSymbol(new Position(6, 6), ConsoleColor.Red, "P"));
+                DrawChessPiece(new PieceSymbol(new Position(7, 6), ConsoleColor.Red, "P"));
+
                
+                DrawChessPiece(new PieceSymbol(new Position(7, 7), ConsoleColor.Red, "R"));
+                DrawChessPiece(new PieceSymbol(new Position(6, 7), ConsoleColor.Red, "H"));
+                DrawChessPiece(new PieceSymbol(new Position(5, 7), ConsoleColor.Red, "B"));
+                DrawChessPiece(new PieceSymbol(new Position(4, 7), ConsoleColor.Red, "Q"));
+                DrawChessPiece(new PieceSymbol(new Position(3, 7), ConsoleColor.Red, "K"));
+                DrawChessPiece(new PieceSymbol(new Position(2, 7), ConsoleColor.Red, "B"));
+                DrawChessPiece(new PieceSymbol(new Position(1, 7), ConsoleColor.Red, "H"));
+                DrawChessPiece(new PieceSymbol(new Position(0, 7), ConsoleColor.Red, "R"));
+
+
 
                 if (position.Y<7)
                 {
