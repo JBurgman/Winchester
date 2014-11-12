d0b4a79a9ce8ae26fc96ddb94eb96624fa3985b3
Emmy Andersson <emmy.andersson.win14@utb.hermods.se>
2014-11-12 13:10:50 +0100

    FilesAndRankstest

--- ChessGameConsoleApplication/FilesRanks.cs
+++ ChessGameConsoleApplication/FilesRanks.cs
@@ -49,40 +49,42 @@ namespace ChessGameConsoleApplication
 
             Console.ForegroundColor = TextColor;
             Console.SetCursorPosition(0, 8);
-            Console.WriteLine("-");
+            Console.WriteLine("─");
             Console.SetCursorPosition(1, 8);
-            Console.WriteLine("-");
+            Console.WriteLine("─");
             Console.SetCursorPosition(2, 8);
-            Console.WriteLine("-");
+            Console.WriteLine("─");
             Console.SetCursorPosition(3, 8);
-            Console.WriteLine("-");
+            Console.WriteLine("─");
             Console.SetCursorPosition(4, 8);
-            Console.WriteLine("-");
+            Console.WriteLine("─");
             Console.SetCursorPosition(5, 8);
-            Console.WriteLine("-");
+            Console.WriteLine("─");
             Console.SetCursorPosition(6, 8);
-            Console.WriteLine("-");
+            Console.WriteLine("─");
             Console.SetCursorPosition(7, 8);
-            Console.WriteLine("-");
+            Console.WriteLine("─");
+            Console.SetCursorPosition(8, 8);
+            Console.WriteLine("┘");
 
 
             //Ranks (1-8)
             Console.SetCursorPosition(8, 0);
-            Console.WriteLine("|8");
+            Console.WriteLine("│8");
             Console.SetCursorPosition(8, 1);
-            Console.WriteLine("|7");
+            Console.WriteLine("│7");
             Console.SetCursorPosition(8, 2);
-            Console.WriteLine("|6");
+            Console.WriteLine("│6");
             Console.SetCursorPosition(8, 3);
-            Console.WriteLine("|5");
+            Console.WriteLine("│5");
             Console.SetCursorPosition(8, 4);
-            Console.WriteLine("|4");
+            Console.WriteLine("│4");
             Console.SetCursorPosition(8, 5);
-            Console.WriteLine("|3");
+            Console.WriteLine("│3");
             Console.SetCursorPosition(8, 6);
-            Console.WriteLine("|2");
+            Console.WriteLine("│2");
             Console.SetCursorPosition(8, 7);
-            Console.WriteLine("|1");
+            Console.WriteLine("│1");
 
 
         }
