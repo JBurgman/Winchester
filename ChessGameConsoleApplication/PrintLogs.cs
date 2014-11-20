41086fdf6d83e4b2ba1fd602ac1cf3b707671bbc
Emmy Andersson <emmy.andersson.win14@utb.hermods.se>
2014-11-20 14:54:12 +0100

    SkatestaGetTileColor

--- ChessGameConsoleApplication/PrintLogs.cs
+++ ChessGameConsoleApplication/PrintLogs.cs
@@ -45,7 +45,7 @@ namespace ChessGameConsoleApplication
             for (int y = 1; y < 6; y++)
             {
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.SetCursorPosition(15, y);
+                Console.SetCursorPosition(20, y);
                 Console.WriteLine(logPost[(logPost.Count - y)]);
             }
 
